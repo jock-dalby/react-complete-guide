d747c27727ddb16799e3ee8acd00727e291a7140
jock-dalby <jockdalby86@gmail.com>
2018-03-13 08:43:47 +1300

    add explanantion of jsx to App Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,25 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+
+    /**
+     * return (
+     *    <div className="App">
+     *      <h1>Hi, I'm a React App</h1>
+     *    </div>
+     * );
+     *
+     * UNDERSTANDING JSX
+     *
+     * The above JSX code is the exact equivalent of the above code and what it will get compiled
+     * into by one of React's many build tools.
+     * This is the reason we need to import 'React' even though we are not using it at all in the
+     * JSX syntax. 'React' will get used when the JSX has been compiled.
+     *
+     * return React.createElement(
+     *    'div', {className: 'App'}, React.createElement(
+     *        'h1', null, "Hi, I'm a React App"));
+     */
   }
 }
 
