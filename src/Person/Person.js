e91e82e1a5efc339cca2feb2c17c4d63740bd64e
jock-dalby <jockdalby86@gmail.com>
2018-03-13 09:41:44 +1300

    add getAge call to person jsx code

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const person = () => {
-    return <p>I'm a person!</p>
+    return <p>I'm a person and I am {getAge()} years old!</p>
+}
+
+const getAge = () => {
+    return Math.floor(Math.random() * 30);
 }
 
 export default person;
