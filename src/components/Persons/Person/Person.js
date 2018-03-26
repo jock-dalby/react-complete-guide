daa10524c510708a2358e25dfd57c3db3e514ced
jock-dalby <jockdalby86@gmail.com>
2018-03-26 18:50:20 +1300

    convert Person and Persons stateless components to be stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-    return (
+class Person extends Component {
+    render() {
+        return (
         <div className={classes.Person}>
-            <p onClick={props.switchAlfiesNameHandler} >I am {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+            <p onClick={this.props.switchAlfiesNameHandler} >I am {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
             <input type="text"
-                value={props.name}
-                onChange={props.nameChangedHandler}/>
-            <button onClick={props.deletePersonHandler}>Delete</button>
+                value={this.props.name}
+                onChange={this.props.nameChangedHandler} />
+            <button onClick={this.props.deletePersonHandler}>Delete</button>
         </div>
-    )
+        )
+    }
 }
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => props.persons.map((person, i) => {
+class Persons extends Component {
+  render () {
+  return this.props.persons.map((person, i) => {
     return <Person
       name={person.name}
       age={person.age}
       index={i}
       key={i}
-      deletePersonHandler={props.clicked.bind(this, i)}
-      nameChangedHandler={(event) => props.changed(event, person.id)} />
+      deletePersonHandler={this.props.clicked.bind(this, i)}
+      nameChangedHandler={(event) => this.props.changed(event, person.id)} />
+    })
   }
-);
+};
 
 export default Persons;
